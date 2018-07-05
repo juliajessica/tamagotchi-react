21d6df3ff885c03d5ac228387445c72997199833
julia sheremet <juliajessicasher@gmail.com>
2018-07-05 15:53:23 -0700

    push to ghpages

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -60,7 +60,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "b3f9537c2d3cd4c8b863"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "aa0e03cb76afd050cc4a"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -707,7 +707,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
